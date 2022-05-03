9be279d3a7772d630ff9a5d364ee2c765b6b2005
Mr-Destructive <gormeet711@gmail.com>
2022-05-03 12:28:22 +0530

    fixed static files

--- capes/urls.py
+++ capes/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     #    path("admin/", admin.site.urls),
     path("mail/", include("mail.urls")),
     path("user/", include("user.urls")),
     path("", include("feeder.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
